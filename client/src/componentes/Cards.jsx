7c9128029341b8f7747db0c1aa07fcfe93f39e05
Lorena Rodriguez <loreleyro.89@gmail.com>
2022-06-02 17:49:45 -0300

    creando paginacion

--- client/src/componentes/Cards.jsx
+++ client/src/componentes/Cards.jsx
@@ -1,32 +1,20 @@
 import { useEffect } from 'react'
-import { useDispatch,  useSelector } from 'react-redux'
-import { traerLosPerritos } from '../redux/actions'
 import Card from './Card'
 
+const Cards = ({listDogs}) => {
 
-
-
-const Cards = () => {
-    const dispatch= useDispatch()
-    const perritos= useSelector((state)=>state.perritos) 
-
-    useEffect(()=>{  
-        dispatch(traerLosPerritos()) 
-    }, [dispatch])
-  
     return (
-    <div>
-        {
-            perritos && perritos.map((element)=>{
-                return(
-                    <div key={element.id}>
-                        <Card name={element.name} image={element.image}/>
-                    </div>
-                )
-                
-            })
-        }
-    </div>
+        <div>
+            {
+                listDogs && listDogs.map((element)=>{
+                    return(
+                            <div key={element.id}>
+                                <Card name={element.name} image={element.image}/>
+                            </div>
+                    ) 
+                })
+            }
+        </div>
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,43 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import { traerLosPerritos } from '../redux/actions'
 import Cards from "../componentes/Cards";
 
 function Home(){
-      
+    const dispatch= useDispatch()
+    const perritos= useSelector((state)=>state.perritos) 
+
+    useEffect(()=>{  
+        dispatch(traerLosPerritos()) 
+    }, [dispatch])
+
+    /** Inicio del paginado */
+    const [currentPage, setCurrentPage]= useState(1)
+
+    const pageSize = 8;
+    const lastIndex= currentPage * pageSize;
+    const firstIndex= lastIndex - pageSize;
+
+    
+    const listDogs = perritos.slice(firstIndex,lastIndex)
+
+    const cambiarPagina=()=>{
+        if(currentPage >= Math.ceil(perritos.length/pageSize)) return;
+
+        setCurrentPage(currentPage +1)
+    }
+
+    const volverAnterior= ()=>{
+        if(currentPage === 1)return;
+        setCurrentPage(currentPage -1)
+    }
+   
+
     return(
         <div>
-            <div >
-              <Cards/>
-            </div>    
+            <button onClick={volverAnterior}>volver</button>
+            <button onClick={cambiarPagina}>siguiente</button>
+            <Cards listDogs={listDogs}/>
         </div>
     )
 }
